83cbe5686861ec8d9d650b299de684eeeda0c88f
carpu <carpuhinnikita@gmail.com>
2024-06-19 23:01:54 +0300

    create part the code

--- main.py
+++ main.py
@@ -15,3 +15,15 @@
 
 
 # импорты
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+async def start():
+    await dp.start_polling(bot)
+# запуск бота через long_polling
+if __name__ == "__main__":
+    asyncio.run(start())
