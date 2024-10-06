b7e480b6a84643589399d4c4cf22c8a7cbd548c1
carpu <carpuhinnikita@gmail.com>
2024-10-06 21:13:21 +0300

    create another new code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
@@ -6,9 +5,8 @@ from aiogram.types import ParseMode
 from aiogram.utils import executor
 from handlers import register_handlers
 from database import init_db
-import os
 
-API_TOKEN = os.getenv("API_TOKEN")
+from config import API_TOKEN
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -27,11 +25,3 @@ async def on_startup(dispatcher):
 if __name__ == '__main__':
     register_handlers(dp)
     executor.start_polling(dp, on_startup=on_startup)
-
-async def on_startup(dp):
-    # Настройка логирования
-    import logging
-    logging.basicConfig(level=logging.INFO)
-
-if __name__ == "__main__":
-    start_polling(dp, on_startup=on_startup)
